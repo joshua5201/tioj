8ee947b0382ada421409731ba97ab093ba624407
Tsung-en Hsiao <joshua841025@gmail.com>
2014-02-01 01:26:43 +0800

    clean old files

--- routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-  var index = 'index'; //setting index page
-  app.get('/', function (req, res) {
-    res.render(index, {title: 'Home'});
-  });
-  require('./controllers/submissions.js')(app);
-  require('./controllers/problems.js')(app);
-}
