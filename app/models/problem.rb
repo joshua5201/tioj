6431fc71df6eb39151bbd45010aba212b6aa7ce2
Tsung-en Hsiao <joshua841025@gmail.com>
2014-02-06 00:26:15 +0800

    update problem.rb

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,7 @@
 class Problem < ActiveRecord::Base
   has_one :limit, :inverse_of => :problem
   has_many :submissions
-  has_many :testdata
+  has_many :testdata, :inverse_of => :problem
 
   accepts_nested_attributes_for :limit, :allow_destroy => true, :reject_if => :all_blank
   accepts_nested_attributes_for :testdata, :allow_destroy => true, :reject_if => :all_blank
