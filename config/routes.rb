6502a0e1bc105ad739fe9a5e5b3a5f9ffd15de7a
Tsung-en Hsiao <joshua841025@gmail.com>
2014-02-10 14:46:48 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Tioj::Application.routes.draw do
     resources :comments
   end
   get 'testdata' => 'testdata#index_all'
+  get 'about' => 'about#index', as: :about
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
