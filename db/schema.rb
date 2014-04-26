ba01e9c941e4a54b90fbc4e99fd84da758c8c1a7
qitar888 <qitar888@gmail.com>
2014-04-26 16:06:03 +0800

    combine show and problem.name

--- db/schema.rb
+++ db/schema.rb
@@ -13,21 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140426061543) do
 
-  create_table "admins", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",    default: 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "comments", force: true do |t|
     t.string   "title"
     t.text     "content"
