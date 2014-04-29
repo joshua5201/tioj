21d29a860474aa05c6a38007411a35414a3b156e
Jonathan <qitar888@github.com>
2014-04-29 16:14:15 +0800

    SQlite3 -> MySQL and some views

--- db/schema.rb
+++ db/schema.rb
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 20140426061543) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
