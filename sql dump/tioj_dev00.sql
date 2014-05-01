75e8d6fc7d2e92e94c4eaeea78e8a15fef81f3a5
silverneko <big2632@gmail.com>
2014-05-01 22:33:48 +0800

    new sql dump, added submissions and td

--- sql dump/tioj_dev00.sql
+++ sql dump/tioj_dev00.sql
@@ -152,7 +152,7 @@ CREATE TABLE `schema_migrations` (
 
 LOCK TABLES `schema_migrations` WRITE;
 /*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
-INSERT INTO `schema_migrations` VALUES ('20140131174639'),('20140131175010'),('20140131175102'),('20140131175153'),('20140201174405'),('20140203101504'),('20140203101934'),('20140203111940'),('20140206093208'),('20140206100424'),('20140206100458'),('20140413143616'),('20140418045046'),('20140426061543');
+INSERT INTO `schema_migrations` VALUES ('20140131174639'),('20140131175010'),('20140131175102'),('20140131175153'),('20140201174405'),('20140203101504'),('20140203101934'),('20140203111940'),('20140206093208'),('20140206100424'),('20140206100458'),('20140413143616'),('20140418045046'),('20140426061543'),('20140430011011');
 /*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -166,15 +166,15 @@ DROP TABLE IF EXISTS `submissions`;
 CREATE TABLE `submissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` text COLLATE utf8_unicode_ci,
-  `compiler` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `result` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `score` int(11) DEFAULT NULL,
+  `compiler` varchar(255) COLLATE utf8_unicode_ci DEFAULT '',
+  `result` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'queued',
+  `score` int(11) DEFAULT '0',
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  `problem_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `problem_id` int(11) DEFAULT '0',
+  `user_id` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -183,6 +183,7 @@ CREATE TABLE `submissions` (
 
 LOCK TABLES `submissions` WRITE;
 /*!40000 ALTER TABLE `submissions` DISABLE KEYS */;
+INSERT INTO `submissions` VALUES (1,'#include<iostream>\r\nusing namespace std;\r\nint main()\r\n{\r\nint n;\r\ncin >> n;\r\nint res = 0;\r\nwhile(n--){\r\nint a;\r\ncin >> a;\r\nres += a;\r\n}\r\ncout << res << endl;\r\nreturn 0;\r\n}','c++','Accepted',NULL,'2014-04-29 12:24:37','2014-05-01 14:25:17',1,NULL),(2,'#include<iostream>\r\nusing namespace std;\r\nint main()\r\n{\r\ncout << 12;\r\nreturn 0;\r\n}','c++','Wrong Answer',0,'2014-05-01 14:13:53','2014-05-01 14:25:13',1,0);
 /*!40000 ALTER TABLE `submissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,7 +202,7 @@ CREATE TABLE `testdata` (
   `updated_at` datetime DEFAULT NULL,
   `problem_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -210,7 +211,7 @@ CREATE TABLE `testdata` (
 
 LOCK TABLES `testdata` WRITE;
 /*!40000 ALTER TABLE `testdata` DISABLE KEYS */;
-INSERT INTO `testdata` VALUES (1,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-04-29 09:18:49','2014-04-29 09:18:49',1),(2,'10\r\n10 20 30 40 50 60 70 80 90 100','550','2014-04-29 09:19:16','2014-04-29 09:19:16',1),(3,'10\r\n4 3 7 8 0 5 2 1 6 9 ','0 1 2 3 4 5 6 7 8 9','2014-04-29 09:19:52','2014-04-29 09:19:52',2),(4,'10\r\n40 30 70 80 00 50 20 10 60 90 ','0 10 20 30 40 50 60 70 80 90','2014-04-29 09:20:46','2014-04-29 09:20:46',2);
+INSERT INTO `testdata` VALUES (1,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-04-29 09:18:49','2014-04-29 09:18:49',1),(2,'10\r\n10 20 30 40 50 60 70 80 90 100','550','2014-04-29 09:19:16','2014-04-29 09:19:16',1),(3,'10\r\n4 3 7 8 0 5 2 1 6 9 ','0 1 2 3 4 5 6 7 8 9','2014-04-29 09:19:52','2014-04-29 09:19:52',2),(4,'10\r\n40 30 70 80 00 50 20 10 60 90 ','0 10 20 30 40 50 60 70 80 90','2014-04-29 09:20:46','2014-04-29 09:20:46',2),(5,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:03','2014-05-01 14:24:03',1),(6,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:09','2014-05-01 14:24:09',1),(7,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:15','2014-05-01 14:24:15',1),(8,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:21','2014-05-01 14:24:21',1),(9,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:27','2014-05-01 14:24:27',1),(10,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:33','2014-05-01 14:24:33',1),(11,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:38','2014-05-01 14:24:38',1),(12,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:44','2014-05-01 14:24:44',1),(13,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:49','2014-05-01 14:24:49',1),(14,'10\r\n1 2 3 4 5 6 7 8 9 10','55','2014-05-01 14:24:54','2014-05-01 14:24:54',1);
 /*!40000 ALTER TABLE `testdata` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -263,4 +264,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-29 17:24:49
+-- Dump completed on 2014-05-01 22:32:34
