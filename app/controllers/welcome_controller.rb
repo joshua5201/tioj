00ad7c43a688a05d0dba07bf50f15deeed86ccb6
Chiang, Yi-Yo <big2632@gmail.com>
2014-07-19 17:34:57 +0800

    change welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
-	def index
-		@problem_last = Problem.last
-		@submission_last = Submission.last
-	end
+  def index
+    @bulletins = Article.all.order("id DESC").limit(7)
+    @contests = Contest.all.order("id DESC").limit(3)
+    @users = User.all.order("id DESC").limit(10)
+    # @problem_last = Problem.last
+    # @submission_last = Submission.last
+  end
 end
