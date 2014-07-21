d2484aabff6020d221051bd3a7104a4030d0a92d
Tsung-en Hsiao <joshua841025@gmail.com>
2014-07-21 16:09:46 +0800

    add user.nickname uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140719110503) do
+ActiveRecord::Schema.define(version: 20140721074647) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -198,6 +198,7 @@ ActiveRecord::Schema.define(version: 20140719110503) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
