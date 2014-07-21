2ddd163c71a23b5cc81d53711bd671521de5e894
Tsung-en Hsiao <joshua841025@gmail.com>
2014-07-21 14:48:19 +0800

    move methods to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @problems = @problems = Problem.all.order("id ASC")
   end
-  def uniq_submits_by_res(res="AC")
-    @submits = @user.submissions.select do |s|
-      s.result == 'AC'
-    end
-    @submits.uniq do |s|
-      s.problem
-    end
-  end
-  def prob_by_res(res="AC")
-    @submits = uniq_submits_by_res(res)
-    @submits.collect do |s|
-      s.problem
-    end
-  end
 end
