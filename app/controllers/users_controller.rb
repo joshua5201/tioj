57e5ab4e21404d86967fe43c61d77afa1061b737
Chiang, Yi-Yo <big2632@gmail.com>
2014-07-21 16:53:11 +0800

    rank

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.sort{|a, b| a.ac_count == b.ac_count ? b.ac_ratio <=> a.ac_ratio : b.ac_count <=> a.ac_count}
+    @users = User.all.sort{|a, b| (a.ac_count == b.ac_count ? b.ac_ratio <=> a.ac_ratio : b.ac_count <=> a.ac_count) }
     @users = Kaminari.paginate_array(@users).page(params[:page]).per(25)
   end
   def show
