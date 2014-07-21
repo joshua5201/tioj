a9d326e9e0c553690a24417548650c1098fefbbe
Chiang, Yi-Yo <big2632@gmail.com>
2014-07-21 15:23:04 +0800

    welcome page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,16 @@ module UsersHelper
     return Submission.select("problem_id").distinct.where("contest_id is NULL AND user_id = ? AND result = ?", user.id, "AC").count
   end
   
+  def ac_ratio_by_user(user)
+    all = user.submissions.where("contest_id is NULL")
+    ac = all.where("result = ?", "AC")
+    all = all.count
+    ac = ac.count
+    ratio = "%.1f%%" % (100.0 * ac / all)
+    
+    return raw ( ratio + " (" + ac.to_s + "/" + all.to_s + ")" )
+  end
+  
   def user_problem_ac(user, problem)
     if Submission.where("contest_id is NULL AND user_id = ? AND problem_id = ? AND result = ?", user.id, problem.id, "AC").count == 0
       return false
