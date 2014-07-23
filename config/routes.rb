243a189b69c12d87f0142a2a8e80c77bc0a5d839
Chiang, Yi-Yo <big2632@gmail.com>
2014-07-23 13:49:06 +0800

    activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Tioj::Application.routes.draw do
 
+  devise_for :users, :controllers => {:registrations => "registrations"}
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, :controllers => {:registrations => "registrations"}
   #resources :limits
   resources :problems do
     resources :testdata
