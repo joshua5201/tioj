5eb4abad783e770779dd95d4918951d37473e515
Chiang, Yi-Yo <big2632@gmail.com>
2014-07-25 21:32:53 +0800

    add markdown with html escape only for posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,22 @@ module ApplicationHelper
     if text == nil
       return 
     end
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, escape_html: false)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
+  
+  def markdown_no_html(text)
+    if text == nil
+      return 
+    end
     renderer = Redcarpet::Render::HTML.new(hard_wrap: true, escape_html: true)
     options = {
       autolink: true,
