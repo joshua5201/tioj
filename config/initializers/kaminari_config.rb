f6eb57bd6296725f0d88149c80c7e2b4e6d4f11d
TIOJ <tioj@tioj.ck.tp.edu.tw>
2014-07-29 15:23:45 +0800

    adjust paginator

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,8 @@
 Kaminari.configure do |config|
    config.default_per_page = 10
    config.max_per_page = nil
-   config.window = 15
-   config.outer_window = 3
+   config.window = 12
+   config.outer_window = 2
    config.left = 0
    config.right = 0
    config.page_method_name = :page
