271c15a341d7aeb79fe0f044748c53eb244276e9
TIOJ <tioj@tioj.ck.tp.edu.tw>
2014-08-04 17:31:11 +0800

    add flash message

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -29,15 +29,15 @@ class ProblemsController < ApplicationController
     unless user_signed_in? && current_user.admin == true
       if @problem.visible_state == 1 
         if params[:contest_id].blank?
-          redirect_to action:'index'
+          redirect_to :back, :notice => 'Insufficient User Permissions.'
           return
         end
         unless @contest.problem_ids.include?(@problem.id) and Time.now >= @contest.start_time and Time.now <= @contest.end_time
-          redirect_to action:'index'
+          redirect_to :back, :notice => 'Insufficient User Permissions.'
           return
         end
       elsif @problem.visible_state == 2
-        redirect_to action:'index'
+        redirect_to :back, :notice => 'Insufficient User Permissions.'
         return
       end
     end
